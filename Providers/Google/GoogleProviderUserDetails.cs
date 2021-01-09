18fb508e2a9b9e8c82aeb170e52b0aa1ad87ee1e
namratapramod <73640277+namratapramod@users.noreply.github.com>
2021-01-09 17:51:22 +0530

    Google file uploaded

---
+++ Providers/Google/GoogleProviderUserDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Invitee.Providers.Google
+{
+    public class GoogleProviderUserDetails
+    {
+        public string Email { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Locale { get; internal set; }
+        public string Name { get; internal set; }
+        public string ProviderUserId { get; internal set; }
+        public string ProfilePic { get; internal set; }
+    }
+}
---
+++ Providers/Google/GoogleTokenVerifier.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace Invitee.Providers.Google
+{
+    public class GoogleTokenVerifier
+    {
+        private const string GoogleApiTokenInfoUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={0}";
+        private static string[] SupportedClientsIds;
+
+        public GoogleProviderUserDetails ValidateGoogleToken(string providerToken)
+        {
+            var httpClient = new HttpClient();
+            var requestUri = new Uri(string.Format(GoogleApiTokenInfoUrl, providerToken));
+
+            HttpResponseMessage httpResponseMessage;
+            try
+            {
+                httpResponseMessage = httpClient.GetAsync(requestUri).Result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            if (httpResponseMessage.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            var googleApiTokenInfo = JsonConvert.DeserializeObject<GoogleApiTokenInfo>(response);
+
+            if (SupportedClientsIds!=null && !SupportedClientsIds.Contains(googleApiTokenInfo.aud))
+            {
+                return null;
+            }
+
+            return new GoogleProviderUserDetails
+            {
+                Email = googleApiTokenInfo.email,
+                FirstName = googleApiTokenInfo.given_name,
+                LastName = googleApiTokenInfo.family_name,
+                Locale = googleApiTokenInfo.locale,
+                Name = googleApiTokenInfo.name,
+                ProviderUserId = googleApiTokenInfo.sub,
+                ProfilePic = googleApiTokenInfo.picture
+            };
+        }
+
+    }
+}
