2fa68bd234d2540b3d08a1c2c25c1b6d1096f7f1
namratapramod <73640277+namratapramod@users.noreply.github.com>
2021-01-09 16:30:51 +0530

    upload basic files

---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Invitee.App_Start;
+using Invitee.Providers;
+using Invitee.Repository;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Microsoft.Owin.Security.Google;
+using FluentScheduler;
+using Invitee.Jobs;
+
+[assembly: OwinStartup(typeof(Invitee.Startup))]
+
+namespace Invitee
+{
+    public class Startup
+    {
+
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions { get; private set; }
+        public static GoogleOAuth2AuthenticationOptions GoogleAuthOptions { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+            var container = AutofacConfig.RegisterComponents();
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AutoMapperConfig.Configure();
+
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            //JobManager.Initialize(new CleanUpUnwantedFilesJob(new RepositoryWrapper(new RepositoryContext()),System.Web.HttpContext.Current));
+            // Database.SetInitializer(new MigrateDatabaseToLatestVersion<RepositoryContext, Migrations.Configuration>());
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+
+            //use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ExternalCookie);
+            OAuthBearerOptions = new OAuthBearerAuthenticationOptions();
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(90),
+                Provider = new TokenAuthProvider()
+                //RefreshTokenProvider = new RefreshTokenProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "506423016217-p5ta34uidkdumq2nj6hpmd9ebesa1kvu.apps.googleusercontent.com",
+                ClientSecret = "AVta-nf6911CvCAhb9ZhuKTD",
+                Provider = new GoogleAuthProvider()
+            });
+        }
+    }
+}
