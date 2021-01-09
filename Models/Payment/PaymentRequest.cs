25c5e16893ec664e9233fea024dee24eb2d5aea2
namratapramod <73640277+namratapramod@users.noreply.github.com>
2021-01-09 17:44:56 +0530

    payment files

--- Models/Payment/PaymentRequest.cs
+++ Models/Payment/PaymentRequest.cs
@@ -1,1 +1,9 @@
-
+﻿namespace Invitee.Models.Payment
+{
+    public class PaymentRequest
+    {
+        public string orderId { get; set; }
+        public string orderAmount { get; set; }
+        public string orderCurrency { get; set; }
+    }
+}
---
+++ Models/Payment/PaymentResponce.cs
@@ -0,0 +1,9 @@
+﻿namespace Invitee.Models.Payment
+{
+    public class PaymentResponce
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string Cftoken { get; set; }
+    }
+}
