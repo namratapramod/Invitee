231919437602e0c02d8d9771930e42f088f70bc7
namratapramod <73640277+namratapramod@users.noreply.github.com>
2021-01-09 17:42:16 +0530

    files

---
+++ Infrastructure/ApiExceptionHandlerAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using Serilog;
+using System.Web.Http.Filters;
+using Autofac;
+using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
+
+namespace Invitee.Infrastructure
+{
+    public class ApiExceptionHandlerAttribute : ExceptionFilterAttribute
+    {
+        private readonly ILogger logger;
+        public ApiExceptionHandlerAttribute()
+        {
+            var log = DependencyResolver.Current.GetService(typeof(ILogger)) as ILogger;
+            this.logger = log.ForContext(this.GetType());
+        }
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+
+            var exType = actionExecutedContext.Exception.GetType();
+
+            if (exType == typeof(DbEntityValidationException))
+            {
+                var errorMessages = ((DbEntityValidationException)actionExecutedContext.Exception).EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+                actionExecutedContext.Exception = new DbEntityValidationException(string.Join("\n", errorMessages), ((DbEntityValidationException)actionExecutedContext.Exception).EntityValidationErrors);
+            }
+            else if(exType == typeof(DbUpdateException))
+            {
+                var errorMessage = ((DbUpdateException)actionExecutedContext.Exception).InnerException;
+                actionExecutedContext.Exception = new Exception(errorMessage.Message.Contains("inner exception")?errorMessage.InnerException.Message : errorMessage.Message);
+            }
+            logger.Error(actionExecutedContext.Exception, "Global Exception Handled - Action Name " + actionExecutedContext.ActionContext.ActionDescriptor.ActionName);
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
---
+++ Infrastructure/BaseApiController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace Invitee.Infrastructure
+{
+    public class BaseApiController : ApiController
+    {
+        public BaseApiController()
+        {
+
+        }
+        protected IHttpActionResult CreateResponse<T>(T content)
+        {
+            if (!ModelState.IsValid)
+                return ModelError();
+            else
+                return Success(content);
+        }
+
+        protected int GetUserId()
+        {
+            return Convert.ToInt32(((ClaimsIdentity)User.Identity).Claims.SingleOrDefault(x => x.Type == "Id").Value);
+        }
+
+        protected string GetDeviceId(Repository.IRepositoryWrapper repositoryWrapper)
+        {
+            var userId = this.GetUserId();
+            return repositoryWrapper.User.FindAll().Single(x => x.Id == userId).DeviceId;
+        }
+
+        protected string GetUserName()
+        {
+            return ((ClaimsIdentity)User.Identity).Name;
+        }
+
+        protected IHttpActionResult ModelError()
+        {
+            return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState));
+        }
+
+        protected IHttpActionResult Error(string message)
+        {
+            return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.BadRequest,message));
+        }
+
+        protected IHttpActionResult Success<T>(T content)
+        {
+            return Ok(content);
+        }
+    }
+}
